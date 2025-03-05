a70da0abd6a39aac14b8190f002f3e6f1020697f
Malinda-invos <malinda@invos.global>
2025-03-05 22:17:28 +0530

    completed Recap part

---
+++ src/sample.ts
@@ -0,0 +1,30 @@
+// import React from "react";
+
+// const App = () => {
+//   let name = "world";
+//   let x = 10;
+//   let y = 20;
+//   const nameArr = ["A", "B", "C"];
+//   const login = true;
+
+//   return (
+//     <div>
+//       <h1 className="text-3xl text-red-500 font-bold font-display">
+//         Hello Invos Global
+//       </h1>
+//       <p>Hello {name}</p>
+//       <p>
+//         The sum of {x} and {y} is {x + y}
+//       </p>
+//       <ul>
+//         {nameArr.map((name, index) => (
+//           <li key={index}>{name}</li>
+//         ))}
+//       </ul>
+//       {login ? <p>You are logged in</p> : <p>You are not logged</p>}
+//       {login && <p>You are logged in</p>}
+//     </div>
+//   );
+// };
+
+// export default App;
