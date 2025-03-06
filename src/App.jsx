7be33b19297370e0a3c27e3ff6eb5a43be9c16d4
Malinda-invos <malinda@invos.global>
2025-03-06 08:45:24 +0530

    basic re-structuring is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCard from "./components/HomeCard";
+import JobListings from "./components/JobListings";
+import ViewAll from "./components/ViewAll";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCard />
+      <JobListings />
+      <ViewAll />
+    </>
+  );
 };
 
 export default App;
